872f4f328f7035d6ae91ca690d43cc3090a040fc
SA3908 <155651747+SA3908@users.noreply.github.com>
2026-05-23 15:41:43 +0100

    Implemented basics of Game IIFE

--- index.js
+++ index.js
@@ -11,24 +11,32 @@ function gameBoard() {
             board[i].push(cell());
         }
     }
-    const playerOne = createPlayer("Player 1");
-    const playerTwo = createPlayer("Player 2");
-
-
 
 }
+
+const Game = (() => { //IIFE function
+    let players = []
+    let currentPlayerIndex = 0;
+
+    const start = () => {
+        players = [
+            createPlayer(document.querySelector("#player1").value, "X"),
+            createPlayer(document.querySelector("#player2").value, "O")
+        ]
+        gameOver = false;
+    }
+})();
 
 function cell() { //either a 0 for empty, 1 for player one and 2 for player 2
     let value = 0;
     return 0;
 }
 
-function createPlayer(name) {
-    const userName = name;
-    return {userName};
+const createPlayer = (name, mark) => { //Player factory
+    return {name, mark}
 }
 
 const startButton = document.querySelector("#start-button");
 startButton.addEventListener("click", () => {
-    
+    Game.start();
 })

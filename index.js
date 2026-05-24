4531b7bd2a760d4cc36c265e7d2b18508ddf3fbb
SA3908 <155651747+SA3908@users.noreply.github.com>
2026-05-24 15:38:18 +0100

    Added displayBoard function to display to console

--- index.js
+++ index.js
@@ -13,8 +13,16 @@ function gameBoard() {
     }
 
     const getBoard = () => board;
+    const setBoardValue = (i, j, marker) => {
+        board[i][j].setValue(marker);
+    }
 
-    return {getBoard};
+    const displayBoard = () => { //displays the game board to console
+        const display = board.map((row) => row.map((Cell) => Cell.getValue())); //Array of Cell values
+        console.log(display);
+    }
+
+    return {getBoard, setBoardValue, displayBoard};
 }
 
 const Game = (() => { //IIFE function
@@ -27,13 +35,16 @@ const Game = (() => { //IIFE function
             createPlayer(document.querySelector("#player1").value, "X"),
             createPlayer(document.querySelector("#player2").value, "O")
         ]
+        board = gameBoard();
+        board.displayBoard();
         gameOver = false;
+
         
     }
     return {start};
 })();
 
-function Cell() { //0 for empty, X for player one, O for player 2
+function Cell() { // return cell object 0 for empty, X for player one, O for player 2
     let value = 0;
     const setValue = (marker) => {
         value = marker;
